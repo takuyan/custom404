75999795520de3a082a63ada648adec6fcadf9f1
takuyan <fanciful.dream@gmail.com>
2011-05-28 16:42:05 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Custom404::Application.config.secret_token = 'bfe2f62c7958cca930c469b0598affda923f3e4dd38478473763366ee4226c9e82cae797c34b5f487e376a83dba3f9306f1ac2d413ebd11e57ff34ad9eeffe81'
